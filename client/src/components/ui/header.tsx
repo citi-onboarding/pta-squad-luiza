011635445f809bdbd1612da1d29eeca04b3fdcc8
IsabelleTenorio <isabelle.tenorio05@gmail.com>
2025-05-22 15:05:26 -0300

    hotfix(feature/header): passed the "styles" to tailwind and created the border changing buttons

--- client/src/components/ui/header.tsx
+++ client/src/components/ui/header.tsx
@@ -1,4 +1,6 @@
-import react from 'react';
+"use client";
+import React from 'react';
+import { useState } from 'react';
 import { LogoCITiPet, LogotipoCITi } from '@/assets';
 import { Favorite } from '@/assets';
 import "@/styles/fonts.css";
@@ -17,48 +19,50 @@ export default function Header({
     text3, 
     text4 
 }: HeaderProps) {
+    const [isAppointment, setIsAppointment] = useState(true);
+    const [isRegistration, setIsRegistration] = useState(false);
+
+    const handleTabChange = (tab:string) => {
+        setIsAppointment(tab === 'appointment');
+        setIsRegistration(tab === 'registration');
+    };
     return (
-        <div style = {{ // Container principal
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            width: '100%',
-            height: '114px',
-            borderBottom: '1px solid rgba(217, 217, 217, 1)',
-            paddingLeft: '48px',
-            paddingRight: '48px',
-            fontFamily: 'SF Pro Display, sans-serif',
-            fontSize: '16px',
-            fontWeight: '400',
-        }}> 
+        <div className='flex flex-row flex-wrap items-center justify-between w-full h-[114px] border-b border-[rgba(217,217,217,1)] px-12 font-sfpro text-base font-normal'> 
             <p style = {{display: 'flex', justifyContent: 'flex-start', paddingBottom: '20px', paddingTop: '20px'}} >
                 <Image src = {LogoCITiPet} alt = "logo" />
             </p>
 
-            <div style = {{display: 'flex', width: '196px', height: '42px', gap: '48px'}}>
-                <div style={{width: '86px', height:'42px', paddingTop: '12px', 
-                    paddingBottom: '12px', borderBottom: '2px solid rgba(80, 230, 120, 1)'}}>
-                    <p style = {{color: '#242424'}}>{text1}</p>
-                </div>
-                <div style = {{width: '62px', height:'42px', paddingTop: '12px', paddingBottom: '12px'}}>
-                    <p style = {{color: '#242424'}}>{text2}</p>
-                </div>
+            <div className='flex w-48 h-[42px] gap-12'>
+                <button
+                    onClick={() => handleTabChange('appointment')}
+                    className={`w-auto h-[42px] py-3 border-b-2 ${
+                        isAppointment ? 'border-[#50E678]' : 'border-transparent'
+                    }`}
+                >
+                    <p className='text-[#242424]'>{text1}</p>
+                </button>
+                <button 
+                    onClick={() => handleTabChange('registration')}
+                    className={`w-auto h-[42px] py-3 border-b-2 ${
+                        isRegistration ? 'border-[#50E678]' : 'border-transparent'
+                    }`}>
+                
+                    <p className='text-[#242424]'>{text2}</p>
+                </button>
             </div>
 
-            <div style = {{width: '240px', height: '24px', gap: '4px', color: '#7D1AD7', display: 'flex', flexWrap: 'wrap', flexDirection: 'row', alignContent: 'center'}}>
+            <div className='w-60 h-6 gap-1 text-[#7D1AD7] flex flex-wrap flex-row content-center'>
                 <div>
                     <p>{text3}</p>
                 </div>
-                <div style = {{paddingTop: '0px', paddingBottom: '3px', paddingLeft: '2px', paddingRight: '2px'}}>
-                    <Image src = {Favorite} alt = "favorite" style = {{width: '24px', height: '24px'}} />
+                <div className='pb-[3px] px-[2px]'>
+                    <Image src = {Favorite} alt = "favorite" className='w-6 h-6' />
                 </div>
                 <div>
                     <p>{text4}</p>    
                 </div>
                 <div>
-                    <Image src = {LogotipoCITi} alt = "logotipo" style = {{width: '41px', height: '21px'}} />
+                    <Image src = {LogotipoCITi} alt = "logotipo" className='w-[41px] h-[21px]' />
                 </div>
             </div>
         </div>
