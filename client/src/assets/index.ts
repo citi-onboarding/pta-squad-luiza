123ca2212aff2637ebb2397ced88eb9915ba5d17
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-05-23 18:31:11 -0300

    hotfix: correcting assets error

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -6,6 +6,7 @@ import LogotipoCITi from "./Logotipo CITi 1.svg";
 import Alarm from "./alarm.svg";
 import Calendar from "./calendar_month.svg";
 import alarmIcon from "./alarmIcon.png";
+import cat from "./cat.png"
 import ArrowDown from "./keyboard_arrow_down_24dp_5F6368_FILL0_wght400_GRAD0_opsz24 2.svg";
-export { LogoCITi, LogoCITiPet, ButtonClose, Favorite, LogotipoCITi, Alarm, Calendar, ArrowDown, alarmIcon };
+export { LogoCITi, LogoCITiPet, ButtonClose, Favorite, LogotipoCITi, Alarm, Calendar, ArrowDown, alarmIcon, cat };
 
